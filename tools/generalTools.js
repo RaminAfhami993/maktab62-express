16fba1d9c99d313c129d6abc2d1bb81b806003d1
RaminAfhami993 <raminafhami.ra@yahoo.com>
2021-12-09 14:20:27 +0330

    add general tools

--- tools/generalTools.js
+++ tools/generalTools.js
@@ -3,7 +3,7 @@ const generalTools = {};
 
 generalTools.randomInteger = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 
 module.exports = generalTools;
