bc6c4b3ed6aac4bdfd87b6d8424cd50abbc4ebe8
RaminAfhami993 <raminafhami.ra@yahoo.com>
2021-12-23 10:42:25 +0330

    delete files

--- tools/generalTools.js
+++
@@ -1,9 +0,0 @@
-const generalTools = {};
-
-
-generalTools.randomInteger = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-
-module.exports = generalTools;
